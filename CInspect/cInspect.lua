47686c1b64ae2a4a1cfc598770a6cfad6e364665
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2021-06-22 13:33:30 -0400

    Update cInspect.lua

--- CInspect/cInspect.lua
+++ CInspect/cInspect.lua
@@ -1,7 +1,8 @@
-
+--Checks to ensure target is able to be inspected and if Control Key is depressed. 
 cInspect = function()
   if(IsControlKeyDown() and UnitIsFriend("player","target") and CanInspect("target") and not InCombatLockdown()) then
     InspectUnit("target") ;
   end
 end
+--Timer that calls the C-Inspect function every 1 Second.
 local myTicker = C_Timer.NewTicker(1, cInspect);
