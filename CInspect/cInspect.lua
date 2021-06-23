4166a877a3675f7d04c14d32a28c9500d0310b16
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2021-06-22 22:44:38 -0400

    Fix for InspectUnit() not fetching gear details.

--- CInspect/cInspect.lua
+++ CInspect/cInspect.lua
@@ -1,13 +1,12 @@
---Checks to ensure target is able to be inspected and if Control Key is depressed.
-
--- If ctrl is held, your mouseover target will be inspected. This includes party/raid frames from within the same zone you're currently in.
--- This works even if the mouseover is not your target, allowing "invisible inspects".
+-- Checks to ensure target is able to be inspected and if Control Key is depressed.
+-- If CTRL + Left Click is used on a friendly target, your mouseover target will be inspected. 
+-- This includes party/raid frames from within the same zone you're currently in.
 cInspect = function()
   if(IsControlKeyDown() and UnitIsFriend("player", "mouseover") and CanInspect("mouseover") and not InCombatLockdown()) then
-    InspectUnit("mouseover");
+    InspectUnit("target"); -- Cannot be mouseover or paperdoll fails to fetch gear details.
   end
 end
---Timer that calls the C-Inspect function every 1 Second(s).
+-- Timer that calls the C-Inspect function every 1 Second(s).
 local myTicker = C_Timer.NewTicker(1, cInspect);
 
 
