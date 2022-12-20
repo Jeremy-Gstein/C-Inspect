d78a323c9b40b3d0e58b3942ad5de1d8f9ee0bdc
Jeremy <66806528+Jeremy-Gstein@users.noreply.github.com>
2022-12-19 21:59:16 -0500

    Removed 'hack' for creating a event via timer and now use a event triggered by the client.

--- C-Inspect.lua
+++ C-Inspect.lua
@@ -1,29 +1,26 @@
 -- Checks to ensure target is able to be inspected and if Control Key is depressed.
--- If CTRL + Left Click is used on a friendly target, your mouseover target will be inspected.
+-- If CTRL + Left Click is used on a friendly target, your target will be inspected.
 -- This includes party/raid frames from within the same zone you're currently in.
 
 -- reload UI when player types ' /rl '
-SLASH_NEWRELOAD1 = /rl
+SLASH_NEWRELOAD1 = "/rl"
 SlashCmdList.NEWRELOAD = ReloadUI
 
--- Keeps track of last player inspected.
-lastInspectTargetName = "" 
 
 cInspect = function()
-  if(IsControlKeyDown() and CanInspect(mouseover) and not InCombatLockdown())then
-    inspectTargetName = UnitName(target);
-    if(inspectTargetName ~= lastInspectTargetName)then
-      InspectUnit(target);
-      lastInspectTargetName = inspectTargetName
-    end
+  if(IsControlKeyDown() and CanInspect("mouseover") and not InCombatLockdown())then
+    InspectUnit("target");       -- Cannot be mouseover or paperdoll fails to fetch gear details.
   end
 end
+-- Define an event to pass cInspect too when Event is called. (checking control key change)
+local function OnEvent(self, event, ...)
+  cInspect();
+end
 
+-- Set Frame to Look for Event and Call cInspect
+local f = CreateFrame("Frame")
+f:RegisterEvent("MODIFIER_STATE_CHANGED")
+f:SetScript("OnEvent", OnEvent)
 
--- Timer that calls the C-Inspect function every 1 Second(s).
-local myTicker = C_Timer.NewTicker(1, cInspect);
-
--- Notes:
--- InspectUnit(target); -- Cannot be mouseover or paperdoll fails to fetch gear details.
 -- ToDo:
--- 1. allow mouseover target to be inspected.
+-- Remove need to press left click or target when inspecting a player.
